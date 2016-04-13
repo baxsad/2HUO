b9af5e3a434952d17abb199c12b090131b4b790d
iURCoder <286363198@qq.com>
2016-04-13 10:13:20 +0800

    🍊

---
+++ 2HUO/Classes/Common/MFJNetWorking/GCDQueue.h
@@ -0,0 +1,13 @@
+//
+//  GCDQueue.h
+//  2HUO
+//
+//  Created by iURCoder on 4/12/16.
+//  Copyright © 2016 iUR. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GCDQueue : NSObject
+
+@end
